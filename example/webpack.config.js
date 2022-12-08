eaef75c8fc7e7b7cd8d02a3d655be54b307af62e
Dercilio Fontes <dercilioafontes@gmail.com>
2022-12-08 18:33:23 -0500

    Updating example project

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,5 +1,5 @@
-var webpack = require('webpack');
 var path = require('path');
+
 module.exports = {
   mode: 'development',
   entry: path.resolve(__dirname, 'app'),
@@ -7,9 +7,6 @@ module.exports = {
     path: __dirname + '/dist',
     publicPath: '/',
     filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'public')
   },
   module: {
     rules: [
