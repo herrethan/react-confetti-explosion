0d617f9bc62420b465ef5091c2f5d63cc76fd04c
Dercilio Fontes <dercilioafontes@gmail.com>
2022-12-08 20:37:55 -0500

    Changed relative react-confetti-explosion dependency

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -6,12 +6,15 @@ module.exports = {
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'public'),
   },
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /(\.css)$/, loader: 'css-loader' }
-    ]
-  }
+      { test: /(\.css)$/, loader: 'css-loader' },
+    ],
+  },
 };
