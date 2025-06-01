2445dd080a2354166b6e535d269a2d1722596b66
Ethan Herr <herrethan@gmail.com>
2025-06-01 18:38:19 -0400

    version 3 baby

--- index.tsx
+++ index.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Example from './src/example';
+
+import { ConfettiExplosion } from './src/confetti';
+
+export const Example = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        fontFamily: 'monospace',
+      }}
+    >
+      <h1>Hello Explosion!</h1>
+      <div style={{ width: 10, height: 10, borderRadius: '100%', border: '2px solid grey' }}>
+        <ConfettiExplosion portal={false} />
+      </div>
+    </div>
+  );
+};
 
 const container = document.getElementById('root');
 if (!container) throw new Error('Failed to find the root element');
